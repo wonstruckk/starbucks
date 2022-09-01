695e638b9c83bd83bac1c5660537fda7e45fa6b8
wonstruckk <wien0526@gmail.com>
2022-09-01 23:32:20 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,27 @@
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      
+      function onYouTubeIframeAPIReady() {
+        //id  = player
+        new YT.Player('player', {
+          videoId: 'An6LvWQuj_8', //최초 재생할 유튜브영상아이디
+          playerVars:{
+            autoplay:true,
+            loop:true,
+            playlist:'An6LvWQuj_8'
+          },
+          events : {
+            onReady: function(event){
+                event.target.mute()
+            }
+          }
+
+        });
+      }     
